70470051e51948830195316d15aad947f2805a21
mu88 <4560672+mu88@users.noreply.github.com>
2020-04-04 16:31:06 +0200

    Implement Store service

---
+++ ServiceLayer/Concrete/StoreService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using DataLayer.EfClasses;
+using DTO.Store;
+
+namespace ServiceLayer.Concrete
+{
+    public class StoreService : IStoreService
+    {
+        public StoreService(SimpleCrudHelper simpleCrudHelper)
+        {
+            SimpleCrudHelper = simpleCrudHelper;
+        }
+
+        private SimpleCrudHelper SimpleCrudHelper { get; }
+
+        /// <inheritdoc />
+        public IEnumerable<ExistingStoreDto> GetAllStores()
+        {
+            return SimpleCrudHelper.GetAllAsDto<Store, ExistingStoreDto>();
+        }
+    }
+}
---
+++ ServiceLayer/IStoreService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using DTO.Store;
+
+namespace ServiceLayer
+{
+    public interface IStoreService
+    {
+        IEnumerable<ExistingStoreDto> GetAllStores();
+    }
+}
