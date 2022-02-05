0035931545b606d8dd57accedd43f502ae707c63
mu88 <4560672+mu88@users.noreply.github.com>
2022-02-05 18:21:33 +0100

    Add HTTPS support

--- ShopAndEat/Program.cs
+++ ShopAndEat/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 using DataLayer.EF;
 using DataLayer.EfClasses;
 using Microsoft.AspNetCore.Hosting;
@@ -12,6 +14,27 @@ public class Program
 {
     public static void Main(string[] args)
     {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            System.IO.File.Copy("mu88_root_CA.crt", "/usr/local/share/ca-certificates/mu88_root_CA.crt");
+                
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "update-ca-certificates",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = false,
+                }
+            };
+            process.Start();
+            var standardOutput = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+
+            Console.WriteLine(standardOutput);
+        }
+
         var host = CreateHostBuilder(args).Build();
 
         CreateDbIfNotExists(host);
